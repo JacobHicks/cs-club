e8610255920b0bbb358bd054f35c353c35621e19
s203295 <jacobphicks@gmaill.com>
2021-02-09 15:27:52 -0600

    i

---
+++ api/name/[name].js
@@ -0,0 +1,21 @@
+const allowCors = fn => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    );
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return
+    }
+    return await fn(req, res)
+};
+
+module.exports = allowCors((req, res) => {
+    const {query: {name}} = req;
+    console.log(name);
+    if(name) res.end(name + " is cool.");
+    else res.status(400).end("Invalid Syntax");
+});
