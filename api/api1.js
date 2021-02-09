b90d92498686b2a9b08063bfe49bea3d8e8d4b97
s203295 <jacobphicks@gmaill.com>
2021-02-09 11:56:03 -0600

    B

--- api/api1.js
+++ api/api1.js
@@ -1,6 +1,6 @@
 module.exports = (req, res) => {
     const {body} = req;
     console.log(body);
-    if(body) res.end(body.name + " is cool.");
-    else res.status(400);
+    if(body && body.name) res.end(body.name + " is cool.");
+    else res.status(400).end("Invalid Syntax");
 };
