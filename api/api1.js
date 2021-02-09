f31a4d90ac1951867d80c9587b78ca47bd4c722f
s203295 <jacobphicks@gmaill.com>
2021-02-09 11:53:43 -0600

    B

--- api/api1.js
+++ api/api1.js
@@ -2,5 +2,5 @@ module.exports = (req, res) => {
     const {body} = req;
     console.log(body);
     if(body) res.end(body.name + " is cool.");
-    else res.end(400);
+    else res.status(400);
 };
